85189f822a5dfe9320da364ee24f26bcd5b82811
fYoghurt <68772131+fYoghurt@users.noreply.github.com>
2022-06-10 23:30:47 +0800

    Update EverPhotoCheckin.py

--- EverPhotoCheckin.py
+++ EverPhotoCheckin.py
@@ -10,9 +10,16 @@ def checkin(token):
         "authorization": f"Bearer {token}",
     }
     res = json.loads(requests.post(url, headers=headers).text)
+    continuity = res['data']['continuity']  # 连续签到天数
+    total_reward = res['data']['total_reward']//1024//1024  # 总奖励空间
+    tomorrow_reward = res['data']['tomorrow_reward']//1024//1024  # 明日奖励
     print(res)
+    print(f'签到成功! 已连续签到: {continuity}天, 共获得: {total_reward}MB, 明日奖励: {tomorrow_reward}MB')
+    if res['data']['checkin_result']==True:
+        print(f'签到成功!')
     if res['data']['checkin_result']!=True:
-        raise Exception('Check-in ERROR')
+        print(f'签到失败')
+        # raise Exception('Check-in ERROR')
 
 if __name__ == '__main__':
     checkin(os.environ['token'])
